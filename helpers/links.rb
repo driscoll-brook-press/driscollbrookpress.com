ca08868de6ee44fab649c524549157f783505c5f
Dale Emery <dale@dhemery.com>
2014-09-13 18:07:05 -0700

    Fix cover link from book description

--- helpers/links.rb
+++ helpers/links.rb
@@ -1,5 +1,3 @@
 def link(display, url)
-    the_link = "<a href='#{url}'>#{display}</a>"
-    puts "Made link: #{the_link} from url #{url} and display #{display}"
-    the_link
+    "<a href='#{url}'>#{display}</a>"
 end
