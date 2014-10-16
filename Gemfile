72e376da4f4ab4883769bfc210c223bb12b1b60c
Dale Emery <dale@dhemery.com>
2014-10-16 15:46:15 -0700

    Add middleman-deploy

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "https://rubygems.org"
 
 gem 'middleman'
+gem 'middleman-deploy', '~> 0.3.0'
 gem 'builder'
 gem 'tzinfo'
--- config.rb
+++ config.rb
@@ -3,7 +3,16 @@ site = OpenStruct.new(YAML::load_file(File.join('data', 'site.yaml')))
 Time.zone = site['timezone']
 
 set :layout, :book
-activate :directory_indexes
 set :haml, {ugly: true}
-helpers
 
+activate :deploy do |deploy|
+  deploy.method = :rsync
+  deploy.host   = 'dhemery.com'
+  deploy.user   = 'dhemery'
+  deploy.path   = '/srv/sites/driscollbrookpress.com'
+  deploy.flags  = '-avz -e ssh --delete'
+end
+
+activate :directory_indexes
+
+helpers
