50046c527bbbd1fe11ac6c85f42fd7377a014658
Dale Emery <dale@dhemery.com>
2014-10-16 23:38:15 -0700

    Move host specs to data/host.yaml

--- config.rb
+++ config.rb
@@ -1,15 +1,16 @@
+host = OpenStruct.new(YAML::load_file(File.join('data', 'host.yaml')))
 site = OpenStruct.new(YAML::load_file(File.join('data', 'site.yaml')))
 
-Time.zone = site['timezone']
+Time.zone = site.timezone
 
 set :layout, :book
 set :haml, {ugly: true}
 
 activate :deploy do |deploy|
   deploy.method = :rsync
-  deploy.host   = 'dhemery.com'
-  deploy.user   = 'dhemery'
-  deploy.path   = '/srv/sites/driscollbrookpress.com'
+  deploy.host   = host.domain
+  deploy.user   = host.user
+  deploy.path   = File.join(host.root, site.domain)
   deploy.flags  = '-avz -e ssh --delete'
 end
 
