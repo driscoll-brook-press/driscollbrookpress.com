bd01d769a3f540b6975646206e4ab709cf381c9e
Dale Emery <dale@dhemery.com>
2015-08-27 19:30:34 -0700

    All catalog lists sort correctly

--- _plugins/catalog.rb
+++ _plugins/catalog.rb
@@ -1,20 +1,20 @@
 module Catalog
-  AVAILABLE = ['new', 'now']
+  AVAILABLE = %w(new now)
 
   def available_now(books)
-    books.select{ |book| AVAILABLE.include? book.data['availability'] }
-      .sort_by { |book| book.data['title'] }
-      .reverse
+    books.select { |book| AVAILABLE.include? book.data['availability'] }
+      .sort_by { |book| book.data['sort'] || book.data['title'] }
   end
 
   def coming_soon(books)
-    books.select{ |book| book.data['availability'] == 'soon' }
+    books.select { |book| book.data['availability'] == 'soon' }
       .sort_by { |book| book.data['date'] }
   end
 
   def new_releases(books)
-    books.select{ |book| book.data['availability'] == 'new' }
+    books.select { |book| book.data['availability'] == 'new' }
       .sort_by { |book| book.data['date'] }
+      .reverse
   end
 end
 
