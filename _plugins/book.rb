bf7e3df70466f71549abda2a0f13ff3e950be957
Dale Emery <dale@dhemery.com>
2016-10-14 14:08:26 -0700

    Improve HTML validation

--- _plugins/book.rb
+++ _plugins/book.rb
@@ -1,21 +1,21 @@
 module Book
-  def book_link book
-    "<a href='#{book.url}'>#{book['title']}</a>"
+  def book_link(book)
+    %Q{<a href="#{book.url}">#{book['title']}</a>}
   end
 
-  def with_slug books, slug
+  def with_slug(books, slug)
     books.find { |book| slug == self.slug(book.url) }
   end
 
-  def slug book_url
+  def slug(book_url)
     book_url.split('/').last.split('.').first
   end
 
-  def cover_image book
-    "<img class='cover-image' src='#{cover_url(book)}' />"
+  def cover_image(book)
+    %Q{<img class="cover-image" src="#{cover_url(book)}" alt="Cover image for #{book['title']}"/>}
   end
 
-  def cover_url book
+  def cover_url(book)
     '/images/%s-cover-web.jpg' % self.slug(book['url'])
   end
 end
